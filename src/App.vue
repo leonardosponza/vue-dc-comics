d62c1fa0da8dc8bf428948de8db6ad5692e0d8bf
leonardo <sponzal4@gmail.com>
2021-07-06 18:47:15 +0200

    header

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header/>
+    <main>
+      <Products/>
+    </main>
+
+    <Footer/>
+   
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+ import Header from './components/Header.vue'
+ import Products from './components/Products.vue'
+ import Footer from './components/Footer.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Products,
+    Footer
   }
 }
 </script>
---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <header>
+      <a href="#">
+          <img src="../assets/dc-logo.png" alt="">
+      </a>
+      <nav>
+          <ul>
+              <li><a href="#">Characters</a></li>
+              <li><a href="#">comics</a></li>
+              <li><a href="#">movies</a></li>
+              <li><a href="#">tv</a></li>
+              <li><a href="#">games</a></li>
+              <li><a href="#">collectibles</a></li>
+              <li><a href="#">videos</a></li>
+              <li><a href="#">fans</a></li>
+              <li><a href="#">news</a></li>
+              <li><a href="#">shop</a></li>
+          </ul>
+      </nav>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
