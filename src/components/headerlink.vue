7a3c925d87539abab9ac7c5e73baafe8711ed77c
leonardo <sponzal4@gmail.com>
2021-07-07 17:10:32 +0200

    props

--- src/components/headerlink.vue
+++ src/components/headerlink.vue
@@ -1,6 +1,6 @@
 <template>
    <header>
-       <div v-for="header in header">
+       <div v-for="header in header" key="header.image.text">
            :img="header.image"
            :Text="header.text"
        </div>
@@ -13,6 +13,7 @@ export default {
         return{
             header: [
                 {
+                require('../assets/dc-logo.png'),
                   image:"../assets/dc-logo.png",
                   text: "Characters comics movies tv games collectibles videos fans news shop" 
      
