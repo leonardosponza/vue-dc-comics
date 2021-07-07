7613b54d1df1e2f51280e34217d480984e28a03e
leonardo <sponzal4@gmail.com>
2021-07-07 17:00:11 +0200

    props

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,28 +1,16 @@
 <template>
-  <header>
-      <a href="#">
-          <img src="../assets/dc-logo.png" alt="">
-      </a>
-      <nav>
-          <ul>
-              <li><a href="#">Characters</a></li>
-              <li><a href="#">comics</a></li>
-              <li><a href="#">movies</a></li>
-              <li><a href="#">tv</a></li>
-              <li><a href="#">games</a></li>
-              <li><a href="#">collectibles</a></li>
-              <li><a href="#">videos</a></li>
-              <li><a href="#">fans</a></li>
-              <li><a href="#">news</a></li>
-              <li><a href="#">shop</a></li>
-          </ul>
-      </nav>
-  </header>
+    <div>
+        <img :src="img" />
+        <p>{{Text}}</p>
+    </div>
 </template>
 
 <script>
 export default {
-
+    props:{
+        img: String,
+        Text: String
+    }
 }
 </script>
 
---
+++ src/components/headerlink.vue
@@ -0,0 +1,28 @@
+<template>
+   <header>
+       <div v-for="header in header">
+           :img="header.image"
+           :Text="header.text"
+       </div>
+   </header>
+</template>
+
+<script>
+export default {
+    data() {
+        return{
+            header: [
+                {
+                  image:"../assets/dc-logo.png",
+                  text: "Characters comics movies tv games collectibles videos fans news shop" 
+     
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
